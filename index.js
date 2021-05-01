11f70d44c51a2fb5393fe17777c1ad99541a81e5
David <david@gayerie.dev>
2021-05-02 01:47:06 +0200

    use symbol

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
+const trSymbol = Symbol();
+
 function assemble(strings, ...args) {
     for(let i = 0; i < args.length; ++i) {
         strings.splice(2 * i + 1, 0, args[i]);
-    }
+    }    
     return strings.join('');
-}
+}    
 
 const tr = function(strings, ...args) {
     const msg = strings.join('${}');
-    const translatedMessage = tr._translations[msg];
+    const translatedMessage = tr[trSymbol][msg];
     if (! translatedMessage) {
         return assemble([...strings], args);
     } else {
         return assemble(translatedMessage, args);
-    }
-}
+    }    
+}    
 
-tr._translations = {};
+tr[trSymbol] = {};
 
 tr.append = function(translations) {
-    Object.assign(tr._translations, translations);
+    Object.assign(tr[trSymbol], translations);
     for (const key in translations) {
-        tr._translations[key] = tr._translations[key].split(/\$\{\d+\}/);
+        tr[trSymbol][key] = tr[trSymbol][key].split(/\$\{\d+\}/);
     }
 }
 
